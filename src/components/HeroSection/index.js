f7218606e11c788a128d93046272e9d908cbac53
Yavuz Tokus <yavuztokus2010@gmail.com>
2021-08-15 19:26:35 +0100

    HeroSection created

---
+++ src/components/HeroSection/index.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react'
+import { MdRadioButtonChecked } from 'react-icons/md';
+import Video from '../../videos/video.mp4';
+import { Button } from '../ButtonElement';
+import { HeroBg, HeroContainer, VideoBg,
+HeroContent, HeroH1, HeroP, HeroBtnWrapper, ArrowForward, ArrowRight, 
+} from './HeroElements';
+
+
+
+
+
+const HeroSection = () => {
+    const [hover, setHover] = useState(false);
+
+    const onHover = () => {
+        setHover(!hover);
+    }
+
+    return (
+        <HeroContainer>
+            <HeroBg>
+                <VideoBg autoPlay loop muted src=
+                {Video} type='video/mp4' />
+            </HeroBg>
+            <HeroContent>
+                <HeroH1>I am a Frontend Developer</HeroH1>
+                    <HeroP>
+                       Sign up to start your software developer career
+                       HTML, CSS, JavaScript
+                    </HeroP>
+              <HeroBtnWrapper>
+                  <Button to="signup"
+                  onMouseEnter={onHover}
+                  onMouseLeave={onHover}
+                  >
+Get Started {hover ? <ArrowForward /> : <ArrowRight />}
+                  </Button>
+              </HeroBtnWrapper>
+            </HeroContent>
+        </HeroContainer>
+    )
+}
+
+export default HeroSection;
