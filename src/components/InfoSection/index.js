d5656650402c33a5acf404b87cd0903a1af29c61
Yavuz Tokus <yavuztokus2010@gmail.com>
2021-08-21 11:09:20 +0100

    InfoSection running

--- src/components/InfoSection/index.js
+++ src/components/InfoSection/index.js
@@ -1,28 +1,45 @@
 import React from 'react'
+import { Button } from 'react-scroll';
+import {
+    InfoContainer,
+    InfoWrapper,
+    InfoRow,
+    Column1,
+    Column2,
+    TextWrapper,
+    TopLine,
+    Heading,
+    Subtitle,
+    BtnWrap,
+    ImgWrap,
+    Img
+} from './InfoElements';
+
 
 const InfoSection = () => {
     return (
         <>
-        <InfoContainer>
-            <InfoWrapper>
-                <InfoRow>
-                    <Column1>
-                        <TextWrapper>
-                            <TopLine>
-                                <Heading>
-                                    <Subtitle>
-                                        <BtnWrap>
-                                            <Button to ="home">Button</Button>
-                                        </BtnWrap>
-                                    </Subtitle>
-                                </Heading>
-                            </TopLine>
-                        </TextWrapper>
-                    </Column1>
-                </InfoRow>
-            </InfoWrapper>
-        </InfoContainer>
-            
+            <InfoContainer>
+                <InfoWrapper>
+                    <InfoRow>
+                        <Column1>
+                            <TextWrapper>
+                                <TopLine>TopLine</TopLine>
+                                <Heading>Heading</Heading>
+                                <Subtitle>Subtitle</Subtitle>
+                                <BtnWrap>
+                                    <Button to ="home" />
+                                </BtnWrap>
+                            </TextWrapper>
+                        </Column1>
+                        <Column2>
+                            <ImgWrap>
+                                <Img />
+                            </ImgWrap>
+                        </Column2>
+                    </InfoRow>
+                </InfoWrapper>
+            </InfoContainer>      
         </>
     )
 }
